d6d9845b425c29e52a93f38e63badb134793051e
Grace Avrilina <grace.aprilina96@gmail.com>
2020-10-23 17:43:27 +0700

    init

---
+++ assets/js/klik.js
@@ -0,0 +1,93 @@
+
+    function changeImage(val) {
+        if (val === "agenda") {
+            document.images["target"].src = "assets/img/fitur/ss/agenda.png";
+            document.images["pic"].src = "assets/img/fitur/btn/wisata.png";
+            document.images["pic2"].src = "assets/img/fitur/btn/agenda2.png";
+            document.images["pic3"].src = "assets/img/fitur/btn/video.png";
+            document.images["pic4"].src = "assets/img/fitur/btn/infokota.png";
+        } 
+        else if (val === "wisata") {
+            document.images["target"].src = "assets/img/fitur/ss/wisata.png";
+            document.images["pic"].src = "assets/img/fitur/btn/wisata2.png";
+            document.images["pic2"].src = "assets/img/fitur/btn/agenda.png";
+            document.images["pic3"].src = "assets/img/fitur/btn/video.png";
+            document.images["pic4"].src = "assets/img/fitur/btn/infokota.png";
+        }
+        else if (val === "video") {
+            document.images["target"].src = "assets/img/fitur/ss/video.png";
+            document.images["pic"].src = "assets/img/fitur/btn/wisata.png";
+            document.images["pic2"].src = "assets/img/fitur/btn/agenda.png";
+            document.images["pic3"].src = "assets/img/fitur/btn/video2.png";
+            document.images["pic4"].src = "assets/img/fitur/btn/infokota.png";
+        }
+        else if (val === "infokota") {
+            document.images["target"].src = "assets/img/fitur/ss/infokota.png";
+            document.images["pic"].src = "assets/img/fitur/btn/wisata.png";
+            document.images["pic2"].src = "assets/img/fitur/btn/agenda.png";
+            document.images["pic3"].src = "assets/img/fitur/btn/video.png";
+            document.images["pic4"].src = "assets/img/fitur/btn/infokota2.png";
+        }
+}
+
+function EchangeImage(val) {
+    if (val === "lapak") {
+        document.images["Etarget"].src = "assets/img/fitur/ss/lapak.png";
+        document.images["Epic"].src = "assets/img/fitur/btn/lapak2.png";
+        document.images["Epic2"].src = "assets/img/fitur/btn/kuliner.png";
+        document.images["Epic3"].src = "assets/img/fitur/btn/kerjaan.png";
+        document.images["Epic4"].src = "assets/img/fitur/btn/properti.png";
+    } 
+    else if (val === "kuliner") {
+        document.images["Etarget"].src = "assets/img/fitur/ss/kuliner.png";
+        document.images["Epic"].src = "assets/img/fitur/btn/lapak.png";
+        document.images["Epic2"].src = "assets/img/fitur/btn/kuliner2.png";
+        document.images["Epic3"].src = "assets/img/fitur/btn/kerjaan.png";
+        document.images["Epic4"].src = "assets/img/fitur/btn/properti.png";
+    }
+    else if (val === "kerjaan") {
+        document.images["Etarget"].src = "assets/img/fitur/ss/kerjaan.png";
+        document.images["Epic"].src = "assets/img/fitur/btn/lapak.png";
+        document.images["Epic2"].src = "assets/img/fitur/btn/kuliner.png";
+        document.images["Epic3"].src = "assets/img/fitur/btn/kerjaan2.png";
+        document.images["Epic4"].src = "assets/img/fitur/btn/properti.png";
+    }
+    else if (val === "properti") {
+        document.images["Etarget"].src = "assets/img/fitur/ss/properti.png";
+        document.images["Epic"].src = "assets/img/fitur/btn/lapak.png";
+        document.images["Epic2"].src = "assets/img/fitur/btn/kuliner.png";
+        document.images["Epic3"].src = "assets/img/fitur/btn/kerjaan.png";
+        document.images["Epic4"].src = "assets/img/fitur/btn/properti2.png";
+    }
+}
+
+function IchangeImage(val) {
+    if (val === "jdwlsolat") {
+        document.images["Itarget"].src = "assets/img/fitur/ss/jdwlsolat.png";
+        document.images["Ipic"].src = "assets/img/fitur/btn/jdwlsolat2.png";
+        document.images["Ipic2"].src = "assets/img/fitur/btn/atm.png";
+        document.images["Ipic3"].src = "assets/img/fitur/btn/polisi.png";
+        document.images["Ipic4"].src = "assets/img/fitur/btn/olahraga.png";
+    } 
+    else if (val === "atm") {
+        document.images["Itarget"].src = "assets/img/fitur/ss/atm.png";
+        document.images["Ipic"].src = "assets/img/fitur/btn/jdwlsolat.png";
+        document.images["Ipic2"].src = "assets/img/fitur/btn/atm2.png";
+        document.images["Ipic3"].src = "assets/img/fitur/btn/polisi.png";
+        document.images["Ipic4"].src = "assets/img/fitur/btn/olahraga.png";
+    }
+    else if (val === "polisi") {
+        document.images["Itarget"].src = "assets/img/fitur/ss/polisi.png";
+        document.images["Ipic"].src = "assets/img/fitur/btn/jdwlsolat.png";
+        document.images["Ipic2"].src = "assets/img/fitur/btn/atm.png";
+        document.images["Ipic3"].src = "assets/img/fitur/btn/polisi2.png";
+        document.images["Ipic4"].src = "assets/img/fitur/btn/olahraga.png";
+    }
+    else if (val === "olahraga") {
+        document.images["Itarget"].src = "assets/img/fitur/ss/olahraga.png";
+        document.images["Ipic"].src = "assets/img/fitur/btn/jdwlsolat.png";
+        document.images["Ipic2"].src = "assets/img/fitur/btn/atm.png";
+        document.images["Ipic3"].src = "assets/img/fitur/btn/polisi.png";
+        document.images["Ipic4"].src = "assets/img/fitur/btn/olahraga2.png";
+    }
+}
